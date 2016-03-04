9f1df0e0e23180a218b8f2331f122b8dfb4fd872
1851847 <1851847@CN265820.ifrn.local>
2016-03-04 15:31:32 -0300

    First Commit!

---
+++ src/Paciente.java
@@ -0,0 +1,62 @@
+
+public class Paciente {
+
+	private double peso;
+	private double altura;
+	
+	//Construtor da Classe
+	Paciente(double peso, double altura){
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	//método para calcular o IMC
+	double CalcularIMC () {
+		double IMC;
+		IMC = (peso / (altura * altura));
+		return IMC;
+	}
+	
+	//método que repassa o diagnostico do paciente
+	String diagnostico() {
+		double IMC;
+		String diagnostico = "null";
+		
+		IMC = this.CalcularIMC();
+				
+		if (IMC < 16) { 
+			diagnostico = "Baixo peso muito grave"; 
+			return diagnostico;
+		}
+		if (IMC < 16.99) { 
+			diagnostico = "Baixo peso grave"; 
+			return diagnostico;
+		}	
+		if (IMC < 18.49) { 
+			diagnostico = "Baixo peso"; 
+			return diagnostico;
+		}		
+		if (IMC < 24.99) { 
+			diagnostico = "Peso Normal"; 
+			return diagnostico;
+		}
+		if (IMC < 29.99) { 
+			diagnostico = "Sobrepeso"; 
+			return diagnostico;
+		}
+		if (IMC < 34.99) { 
+			diagnostico = "Obesidade Grau I"; 
+			return diagnostico;
+		}
+		if (IMC < 39.99) { 
+			diagnostico = "Obesidade Grau II"; 
+			return diagnostico;
+		}
+		if (IMC > 40) { 
+			diagnostico = "Obesidade Grau III - (obesidade mórbida)"; 
+			return diagnostico;
+		}	
+		
+		return diagnostico;
+	}
+}
---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//instanciação e construção dos objetos
+		Paciente paciente1 = new Paciente(84, 1.70);
+		Paciente paciente2 = new Paciente(64, 1.90);
+		Paciente paciente3 = new Paciente(123, 1.70);
+		
+		//Impressão do diagnostico
+		System.out.println("IMC P1 = " + paciente1.CalcularIMC() + " / Diagnostico: " + paciente1.diagnostico()+ "\n");
+		System.out.println("IMC P2 = " + paciente2.CalcularIMC() + " / Diagnostico: " + paciente2.diagnostico()+ "\n");
+		System.out.println("IMC P3 = " + paciente3.CalcularIMC() + " / Diagnostico: " + paciente3.diagnostico()+ "\n");
+		
+	}
+}
